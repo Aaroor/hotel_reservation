41eeff9e6101a8d66e8801a9ed1e3d3e475c6833
Aarooran Kanthasamy <aaroor2015@gmail.com>
2019-02-21 16:04:52 +0530

    Add release 1 fixed bug

--- resources/views/receptionist/check_availability_map.blade.php
+++ resources/views/receptionist/check_availability_map.blade.php
@@ -685,7 +685,26 @@
                                                 @endif
                                                 <td>{{$bookingInfo->from_date}}</td>
                                                 <td>{{$bookingInfo->to_date}}</td>
-                                            
+                                                @if($bookingInfo->check_out_status==0)
+                                                @if($bookingInfo->status==3)
+                                                <td><span style="background-color:green;padding:5px">Not confirmed yet<span></td>
+                                                <td><a style="background-color:green" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Confirm to booking" href="{{route('res_confirm_booking', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-check"></i></a>&nbsp
+                                                    <a style="background-color:red" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Remove Booking" href="{{route('res_remove_not_confirm_booking', ['id'=>$bookingInfo->booking_id])}}"  class="btn btn-light btn--icon-text"><i class="zmdi zmdi-delete"></i></a>
+                                                    </td>
+                                                @elseif($bookingInfo->status==2)
+                                                <td><span style="background-color:green;padding:5px">Confirmed<span></td>
+                                                <td>
+                                                <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Direct To Payment" href="{{route('res_bill_payment', ['id'=>$bookingInfo->invoice_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-money"></i></a>&nbsp
+                                                    <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Check out payment" href="{{route('res_pay_check', ['id'=>$bookingInfo->invoice_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-shopping-cart"></i></a>
+                                                    <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Go to booking list" href="{{route('res_direct_to_booking_list', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-format-list-bulleted"></i></a>
+                                                    <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Make Orders For Meals" href="{{route('res_re_meals_booking', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-cutlery"></i></a>
+                                                    <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Make Booking For Airport Pick Up/Drop" href="{{route('res_re_air_booking', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-car-taxi"></i></a>
+                                                    <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Add another booking for invoice" href="{{route('res_re_room_booking', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" ><i class="zmdi zmdi-hotel"></i></a>
+                                                    <a style="background-color:#ff6600" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Change Booking Confirm Status" href="{{route('res_not_confirm_booking', ['id'=>$bookingInfo->booking_id])}}" class="btn btn-light btn--icon-text" ><i class="zmdi zmdi-check"></i></a>
+
+                                                </td>
+                                                @endif
+                                            @else
                                             <td><span style="background-color:green;padding:5px">Check Out<span></td>
                                                 <td>
                                                 <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Direct To Payment" href="#" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-money"></i></a>&nbsp
@@ -694,7 +713,11 @@
                                                     <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Make Orders For Meals" href="#" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-cutlery"></i></a>
                                                     <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Make Booking For Airport Pick Up/Drop" href="#" class="btn btn-light btn--icon-text" href=""><i class="zmdi zmdi-car-taxi"></i></a>
                                                     <a data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Add another booking for invoice" href="#" class="btn btn-light btn--icon-text" ><i class="zmdi zmdi-hotel"></i></a>
+                                                    <a style="background-color:#ff6600" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Change Booking Confirm Status" href="#" class="btn btn-light btn--icon-text" ><i class="zmdi zmdi-check"></i></a>
+
                                                 </td>
+                                            @endif
+                                            
                                             </tr>
                                             @endforeach
                                     	@endif
